2eb93cdfe00822e1e397a78a7f2b934c37424101
Anamika Mishra <mishraanamika11a@gmail.com>
2026-01-21 18:49:15 +0530

    Fix SECRET_KEY env handling for Render

--- food/settings.py
+++ food/settings.py
@@ -9,25 +9,30 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG") == "True"
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =config('SECRET_KEY') 
+# SECRET_KEY =config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
        
 
 
